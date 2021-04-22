7a426b9a338310476f32baf9bfc5b2a1bf479394
arielsilvestre <77841278+arielsilvestre@users.noreply.github.com>
2021-04-22 15:52:59 -0300

    Add files via upload

--- DDL/1. Tablas.sql
+++ DDL/1. Tablas.sql
@@ -1,27 +1,6 @@
 CREATE DATABASE proyecto_bd2;
 USE proyecto_bd2;
 
-CREATE TABLE rol(
-	id_rol INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	descripcion VARCHAR(100) 
-);
-
-CREATE TABLE cliente(
-	id_cliente INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	id_cc INT NOT NULL, -- ID del Centro de Costos
-	id_cf INT NOT NULL, -- ID del Centro de Facturación
-	nombre_cliente VARCHAR(100)		
-);
-	
-
-CREATE TABLE proyecto(
-	id_proyecto INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	nombre VARCHAR(100),
-	descripcion VARCHAR(100),
-	id_cliente INT NOT NULL,
-	CONSTRAINT fk_id_cliente FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente)
-);
-
 CREATE TABLE persona(
    legajo INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
    nombre_apellido VARCHAR (100),
@@ -33,15 +12,21 @@ CREATE TABLE persona(
    CONSTRAINT fk_id_rol FOREIGN KEY (id_rol) REFERENCES rol(id_rol)
 );
 
-CREATE TABLE asignacion_horas(
-	cantidad_horas INT NOT NULL,
-	fecha_ah DATE, 
-	legajo INT NOT NULL,
-	id_proyecto INT NOT NULL,
-	CONSTRAINT fk_id_persona FOREIGN KEY (legajo) REFERENCES persona(legajo),
-	CONSTRAINT fk_id_pro_ah FOREIGN KEY (id_proyecto) REFERENCES proyecto(id_proyecto)
-);
-   
+CREATE TABLE cliente(
+	id_cliente INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	id_cc INT NOT NULL, -- ID del Centro de Costos
+	id_cf INT NOT NULL, -- ID del Centro de Facturación
+	nombre_cliente VARCHAR(100)		
+	);
+	
+CREATE TABLE proyecto(
+	id_proyecto INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	nombre VARCHAR(100),
+	descripcion VARCHAR(100),
+	id_cliente INT NOT NULL,
+	CONSTRAINT fk_id_cliente FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente)
+	);
+
 CREATE TABLE participacion(
    id_participacion INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	persona VARCHAR(100),
@@ -53,8 +38,21 @@ CREATE TABLE participacion(
    CONSTRAINT fk_id_pro_p FOREIGN KEY (id_proyecto) REFERENCES proyecto(id_proyecto),
    CONSTRAINT fk_id_rol_pa FOREIGN KEY (id_rol) REFERENCES rol(id_rol),
    CONSTRAINT fk_id_legajo FOREIGN KEY (legajo) REFERENCES persona(legajo)
-);
+   );
+
+CREATE TABLE rol(
+	id_rol INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	descripcion VARCHAR(100) 
+	);
 	
+CREATE TABLE asignacion_horas(
+	cantidad_horas INT NOT NULL,
+	fecha_ah DATE, 
+	legajo INT NOT NULL,
+	id_proyecto INT NOT NULL,
+	CONSTRAINT fk_id_persona FOREIGN KEY (legajo) REFERENCES persona(legajo),
+	CONSTRAINT fk_id_pro_ah FOREIGN KEY (id_proyecto) REFERENCES proyecto(id_proyecto)
+	);
 
 CREATE TABLE liquidacion(
    id_liquidacion INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -62,8 +60,7 @@ CREATE TABLE liquidacion(
    id_proy INT NOT NULL, 
    id_cliente INT NOT NULL,
    cantidad_horas INT NOT NULL,
-   mes_liq INT NOT NULL,
-   anio_liq INT NOT NULL,
+   fecha_liq DATE,
    CONSTRAINT fk_id_rol_l FOREIGN KEY (id_rol) REFERENCES rol(id_rol),
    CONSTRAINT fk_id_pro_l FOREIGN KEY (id_proy) REFERENCES proyecto(id_proyecto),
    CONSTRAINT fk_id_cliente_l FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente)
